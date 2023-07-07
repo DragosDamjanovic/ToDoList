8698f81fe0090c2b7fa6c619d301c940b72ce237
dragos damjanovic <dragos@infopuls.net>
2023-07-07 17:14:01 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import {RouterView} from 'vue-router'
+</script>
+
+<style>
+</style>
